322f6b2141676ab2a1916477619e51921f5c9283
Web3 Developer <wonderfulplayer@outlook.com>
2023-05-06 01:23:31 +0800

    Create MultipleCollection.sol

---
+++ old-contracts/ERC1155Marketplace/MultipleCollection.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
+contract MultipleCollection is ERC1155, Ownable, ERC2981 {
+    constructor() ERC1155("") Ownable() {
+        _setDefaultRoyalty(msg.sender, 500);
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, ERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function updateRoaylty(uint96 newRoaylty) external onlyOwner {
+        _setDefaultRoyalty(owner(), newRoaylty);
+    }
+}
